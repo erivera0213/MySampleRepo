80d134a0260854811a8d0286c8d2da9fefe1413d
erivera0213 <38864661+erivera0213@users.noreply.github.com>
2024-03-03 16:00:06 -0600

    Update CalculatorStepDefinitions.cs
    Added addtional comments and removed comments.

--- SpecFlowCalculator.Specs/SpecFlowCalculator.Specs/StepDefinitions/CalculatorStepDefinitions.cs
+++ SpecFlowCalculator.Specs/SpecFlowCalculator.Specs/StepDefinitions/CalculatorStepDefinitions.cs
@@ -20,29 +20,20 @@ namespace SpecFlowCalculator.Specs.StepDefinitions
 
         [Given("the first number is (.*)")]
         public void GivenTheFirstNumberIs(int number)
-        {
-            //TODO: implement arrange (precondition) logic
-            // For storing and retrieving scenario-specific data see https://go.specflow.org/doc-sharingdata
-            // To use the multiline text or the table argument of the scenario,
-            // additional string/Table parameters can be defined on the step definition
-            // method. 
-
+        {            
             _calculator.FirstNumber = number;
         }
 
         [Given("the second number is (.*)")]
         public void GivenTheSecondNumberIs(int number)
         {
-            //TODO: implement arrange (precondition) logic
-
             _calculator.SecondNumber = number;
         }
 
         [When("the two numbers are added")]
         public void WhenTheTwoNumbersAreAdded()
-        {
-            //TODO: implement act (action) logic
-
+        {     
+            //removed remarks
             _result = _calculator.Add();
         }
 
